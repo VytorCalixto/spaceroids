ee057585d83561a9c0e0bf4a1ebfdc0b1cb2d0fc
Vytor Calixto <vytorcalixto@gmail.com>
2017-06-16 00:56:54 -0300

    Add player movement

---
+++ Assets/FollowUV.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUV : MonoBehaviour {
+
+	public float parallax = 10;
+
+	// Update is called once per frame
+	void Update () {
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x = transform.position.x / transform.localScale.x / parallax;
+		offset.y = transform.position.y / transform.localScale.y / parallax;
+
+		mat.mainTextureOffset = offset;
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float movementForce = 10;
+	public float rotation = 100;
+
+	void Update () {
+		if(Input.GetKey("w")) {
+			transform.Translate(Vector3.up * -movementForce * Time.deltaTime, Space.Self);
+		}
+
+		if(Input.GetKey("a")) {
+			transform.Rotate(Vector3.forward * -rotation * Time.deltaTime);
+		}
+
+		if(Input.GetKey("d")) {
+			transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+		}
+
+	}
+}
